0855d7e9f0a7a8220a75773e8c8ffa24ccc72a6b
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-08-12 11:38:56 +0600

    NEW CHANGES: 2025-08-12 11:38:56 AM

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 // test routes
